67f4cef37e10eded50df9ebf51bda40305a5e58b
George McCann <george_mccann7@hotmail.com>
2023-09-14 22:10:27 +0100

    Services ACF fields

--- templates/services.php
+++ templates/services.php
@@ -17,66 +17,34 @@ get_header(); ?>
   <div class="container py-5">
     <div class="row justify-content-center align-items-center">
       <div class="col-12 col-lg-4 text-start">
-        <h2>Our services</h2>
-        <p>At HALO Present we live and breathe presentations!</p>
-        <p>So, whether you need a presentation created from scratch, or you have a deck that is in desperate need of a design refresh, our strategic approach will ensure that your presentation makes a difference every time</p>
+        <h2><?php the_field('our_services_title'); ?></h2>
+        <?php the_field('our_services_content'); ?>
       </div>
 
 
       <div class="col-12 col-lg-8">
         <div class="row service-boxes">
-          <div class="col-12 col-lg-6 mb-4">
-            <a href="#" class="service">
-              <div class="service-title text-center">
-                <img src="/wp-content/uploads/2023/09/PresentationsIcons.png" />
-                <h3>Presentations</h3>
-              </div>
-              
-              <div class="service-info text-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do incididunt ut labore et dolore magna aliqua. </p>
-                <h3>Presentations</h3>
-              </div>
-            </a>
-          </div> 
-          <div class="col-12 col-lg-6 mb-4">
-            <a href="#" class="service">
-              <div class="service-title text-center">
-                <img src="/wp-content/uploads/2023/09/PresentationsIcons.png" />
-                <h3>Presentations</h3>
-              </div>
-              
-              <div class="service-info text-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do incididunt ut labore et dolore magna aliqua. </p>
-                <h3>Presentations</h3>
-              </div>
-            </a>
-          </div> 
-          <div class="col-12 col-lg-6 mb-4">
-            <a href="#" class="service">
-              <div class="service-title text-center">
-                <img src="/wp-content/uploads/2023/09/PresentationsIcons.png" />
-                <h3>Presentations</h3>
-              </div>
-              
-              <div class="service-info text-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do incididunt ut labore et dolore magna aliqua. </p>
-                <h3>Presentations</h3>
-              </div>
-            </a>
-          </div> 
-          <div class="col-12 col-lg-6 mb-4">
-            <a href="#" class="service">
-              <div class="service-title text-center">
-                <img src="/wp-content/uploads/2023/09/PresentationsIcons.png" />
-                <h3>Presentations</h3>
-              </div>
-              
-              <div class="service-info text-center">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do incididunt ut labore et dolore magna aliqua. </p>
-                <h3>Presentations</h3>
-              </div>
-            </a>
-          </div> 
+
+          <?php  
+          if( have_rows('services') ):
+            while( have_rows('services') ) : the_row(); ?>
+              <div class="col-12 col-lg-6 mb-4">
+                <a href="<?php the_sub_field('service_link'); ?>" class="service">
+                  <div class="service-title text-center">
+                    <img src="<?php the_sub_field('service_image'); ?>" alt="<?php the_sub_field('service_title'); ?>"/>
+                    <h3><?php the_sub_field('service_title'); ?></h3>
+                  </div>
+                  
+                  <div class="service-info text-center">
+                  <?php the_sub_field('service_content'); ?>
+                    <h3><?php the_sub_field('service_title'); ?></h3>
+                  </div>
+                </a>
+              </div> 
+            
+            <?php 
+            endwhile;
+          endif; ?>    
         </div>
       </div>
 
@@ -91,67 +59,29 @@ get_header(); ?>
   <div class="container py-5">
     <div class="row justify-content-center align-items-center">
       <div class="col-12 col-lg-6 text-center">
-        <h2>Presentation types</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
+        <h2><?php the_field('presentation_types_title'); ?></h2>
+        <?php the_field('presentation_types_content'); ?>
       </div> 
       <div class="col-12 mt-4">
         <div class="presentation-types-slider text-light mt-4 p-5">
-          <div>
-            <div class="row align-items-center justify-content-between gx-5">
-              <div class="col-12 col-lg-5">
-                <h2 class="text-light">Sales presentation</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+  
+          <?php  
+          if( have_rows('presentation_types_slides') ):
+            while( have_rows('presentation_types_slides') ) : the_row(); ?> 
+              <div>
+                <div class="row align-items-center justify-content-between gx-5">
+                  <div class="col-12 col-lg-5">
+                    <h2 class="text-light"><?php the_sub_field('title'); ?></h2>
+                    <?php the_sub_field('content'); ?>
+                  </div> 
+                  <div class="col-12 col-lg-6">
+                    <img class="w-100" src="<?php the_sub_field('image'); ?>" alt="<?php the_sub_field('title'); ?>" />
+                  </div>
+                </div>
               </div> 
-              <div class="col-12 col-lg-6">
-                <img class="w-100" src="/wp-content/uploads/2023/09/shutterstock_2136489321.png" />
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <div class="row align-items-center justify-content-between gx-5">
-              <div class="col-12 col-lg-5">
-                <h2 class="text-light">Sales presentation</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-              </div> 
-              <div class="col-12 col-lg-6">
-                <img class="w-100" src="/wp-content/uploads/2023/09/shutterstock_2136489321.png" />
-              </div>
-            </div>
-          </div>
-
-
-          <div>
-            <div class="row align-items-center justify-content-between gx-5">
-              <div class="col-12 col-lg-5">
-                <h2 class="text-light">Sales presentation</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-              </div> 
-              <div class="col-12 col-lg-6">
-                <img class="w-100" src="/wp-content/uploads/2023/09/shutterstock_2136489321.png" />
-              </div>
-            </div>
-          </div>
-
-
-          <div>
-            <div class="row align-items-center justify-content-between gx-5">
-              <div class="col-12 col-lg-5">
-                <h2 class="text-light">Sales presentation</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-              </div> 
-              <div class="col-12 col-lg-6">
-                <img class="w-100" src="/wp-content/uploads/2023/09/shutterstock_2136489321.png" />
-              </div>
-            </div>
-          </div>
-
-
-
+            <?php 
+            endwhile;
+          endif; ?>  
         </div>
       </div> 
     </div>
@@ -177,37 +107,31 @@ $(document).ready(function(){
     <div class="row gx-0">
       <div class="col-12 col-lg-6">
         <div class="half-block light text-left">
-          <h2 class="mb-5">Our services</h2>
-          <p>Every presentation we craft is purposefully designed to drive change.</p>
-          <p>Whether it's a standout pitch, an engaging sales presentation or a interactive experience, you can count on us to construct a presentation that connects with your audience and delivers the right results!</p>
+          <h2 class="mb-5"><?php the_field('content_left_title'); ?></h2>
+          <?php the_field('content_left_content'); ?>
           <div class="mt-5">
-            <a href="#" class="btn btn-primary">FIND OUT MORE</a>
+            <a href="<?php the_field('content_left_link'); ?>" class="btn btn-primary">FIND OUT MORE</a>
           </div>
           
         </div>
       </div>
 
       <div class="col-12 col-lg-6">
-        <div class="half-block dark">
- 
+        <div class="half-block dark"> 
           <ol class="fa-ul">
-            <li><span class="fa-li"><i class="fa fa-check"></i></span>Lower rates</li>
-            <li><span class="fa-li"><i class="fa fa-check"></i></span>Support provided for multiple teams</li>
-            <li><span class="fa-li"><i class="fa fa-check"></i></span>Dedicated Account Manager</li>
-            <li><span class="fa-li"><i class="fa fa-check"></i></span>Hours can be offset for any service</li>
-            <li><span class="fa-li"><i class="fa fa-check"></i></span>Reduced admin</li>
-          </ol>
-
-          
+            <?php  
+            if( have_rows('content_right_checkboxes') ):
+              while( have_rows('content_right_checkboxes') ) : the_row(); ?> 
+                <li><span class="fa-li"><i class="fa fa-check"></i></span><?php the_sub_field('check'); ?></li> 
+              <?php 
+              endwhile;
+            endif; ?>   
+          </ol> 
         </div>
       </div> 
     </div>
   </div>
-</section>
-
-
+</section> 
 <?php get_template_part( 'template-parts/content', 'our-work' ); ?>
- 
-
  
 <?php get_footer(); ?>
