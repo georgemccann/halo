834c6a6ec21ffa46372d8c2f6ffdc611fdf39a4d
George McCann <george_mccann7@hotmail.com>
2023-09-15 13:00:47 +0100

    Fix padding on quote block

--- template-parts/content-quote-block.php
+++ template-parts/content-quote-block.php
@@ -1,7 +1,6 @@
-<section id="mid-section" class="mid-section quote p-relative">
-  <img src="<?php echo $background_image; ?>" class="w-100"/>
-  <div class="quote-content">
-    <div class="container">
+<section style="background:url('<?php echo $background_image; ?>'); background-size: cover!important; background-repeat: no-repeat;" id="mid-section" class="mid-section quote p-relative">
+  <div class="quote-content position-relative py-5">
+    <div class="container py-5">
       <div class="row align-items-center justify-content-center">
         <div class="col-12 col-lg-10">
           <p><?php echo $quote; ?></p>
